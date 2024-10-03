3671854ce7c34cf807187bd13f3b789fa98f952b
PANJI <144399081+ILHAMAJJAH@users.noreply.github.com>
2024-10-03 13:55:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+VANTA.BIRDS({
+    el: ".fog",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color1: 0x1e00ff
+  })
+  
